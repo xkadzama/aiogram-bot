2d7c8d9593a5f88196898039cf00eb7e5cbee442
xkadzama <kartoev.husein@gmail.com>
2026-01-11 20:27:19 +0300

    dev: Реализовал код №2

--- middlewares/logging.py
+++ middlewares/logging.py
@@ -4,6 +4,6 @@ from aiogram.middleware import BaseMiddleware
 class LogginMiddleware(BaseMiddleware):
 	async def __call__(self, handler, event, data):
 		await handler()
-
+		print('Какой-то код от разработчика №2')
 
 
