7f37070e9c90f82b0b6cc9bfdfe41056131cc945
Adam Suleymanov <adamduraev05@gmail.com>
2026-01-12 15:16:03 +0300

    main Добавил приветствие

--- middlewares/logging.py
+++ middlewares/logging.py
@@ -1,10 +1,4 @@
-from aiogram.middleware import BaseMiddleware
 
-
-class LogginMiddleware(BaseMiddleware):
-	async def __call__(self, handler, event, data):
-		await handler()
-		print('Какой-то код от разработчика №2')
 
 
 
